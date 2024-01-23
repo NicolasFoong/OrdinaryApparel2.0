11ab39c30231c7b111b99904b8439f2269cef009
Derek <liderek63@gmail.com>
2024-01-23 15:08:08 -0800

    add authentication for signup/login

---
+++ Scripts/signup.js
@@ -0,0 +1,39 @@
+const signupForm = document.querySelector("#signup");
+
+signupForm.onsubmit = async function (event) {
+  // Prevent the form from refreshing the page,
+  // as it will do by default when the Submit event is triggered:
+  event.preventDefault();
+
+  const username = signupForm.username.value;
+  const password = signupForm.password.value;
+  const email = signupForm.email.value;
+
+  // We can use signupForm.username (for example) to access
+  // the input element in the form which has the ID of "username".
+  const userData = {
+    fullName: email,
+    username,
+    password,
+  };
+
+  // Disables the button after the form has been submitted already:
+  signupForm.signupButton.disabled = true;
+
+  signupForm.signupButton.innerHTML = `
+    <div class="spinner-border text-white" role="status">
+      <span class="visually-hidden">Loading...</span>
+      </div>`;
+
+  const response = await createUser(userData);
+  if (!response.ok) {
+    document.getElementById("error-msg").classList.remove("d-none");
+    // Re-enable the button after the fetch request has been completed:
+    signupForm.signupButton.disabled = false;
+    signupForm.signupButton.textContent = "Sign Up";
+    return;
+  } else {
+    // Time to actually process the login using the function from auth.js!
+    await login({ username, password });
+  }
+};
